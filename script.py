2b5e96ac012dda2153a191cb71cfbc30e86bb072
Daniel Xu <niellxu@yahoo.ca>
2019-05-19 14:35:02 -0400

    Initial commit

---
+++ script.py
@@ -0,0 +1,11 @@
+def tree(d, max_depth=-1, include_file=True):
+    """
+    Print tree like structure from the starting directory recursively
+
+    `d` Starting directory
+
+    `max_depth` The max depth that will be traveled, negative numbers represent unlimited
+
+    `include_file` Also print files, default is true
+    """
+    return
