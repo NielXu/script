aaff9a347d49bd19fe8e0364264b5bd109c8efed
Daniel Xu <Daniel.Xu@fleetcomplete.com>
2019-05-24 15:22:25 -0400

    Removed ignore parameter in tree function

--- fscript.py
+++ fscript.py
@@ -5,7 +5,7 @@ import os
 import re
 
 
-def tree(d, max_depth=-1, include_file=True, pretty_print=True, ignore=[]):
+def tree(d, max_depth=-1, include_file=True, pretty_print=True):
     """
     Print tree like structure from the starting directory recursively
 
@@ -16,20 +16,29 @@ def tree(d, max_depth=-1, include_file=True, pretty_print=True, ignore=[]):
     `include_file` Also print files, default is True
 
     `pretty_print` Enable pretty print if True, False otherwise
-
-    `ignore` A list of regex objects or strings, matched files/directories will be ignored.
-    If the directory is ignored, the children files/directories will also be ignored
     """
     if not os.path.isdir(d):
         raise Exception(str(d)+": is not a directory")
-    _recur_read(d, 0, max_depth, include_file, pretty_print, ignore, "     ")
+    if pretty_print:
+        _pretty_read(d, 0, max_depth, include_file, "     ")
+    else:
+        _read(d, 0, max_depth, "     ")
 
 
-def _recur_read(parent, depth, max_depth, include_file, pretty_print, ignore, pad):
-    "Recursively reading files"
+def _read(parent, depth, max_depth, pad):
     if max_depth >= 0 and depth >= max_depth:
         return
-    if _regex_match(parent, ignore):
+    for file in os.listdir(parent):
+        print(pad*depth + file)
+        fullpath = os.path.join(parent, file)
+        if os.path.isfile(fullpath):
+            continue
+        _read(fullpath, depth+1, max_depth, pad)
+
+
+def _pretty_read(parent, depth, max_depth, include_file, pad):
+    "Recursively reading files"
+    if max_depth >= 0 and depth >= max_depth:
         return
     print(pad[0:-5] + "+--- " + os.path.split(parent)[-1])
     files = []
@@ -44,24 +53,12 @@ def _recur_read(parent, depth, max_depth, include_file, pretty_print, ignore, pa
         path = os.path.join(parent, file)
         if os.path.isdir(path):
             if count == len(files):
-                _recur_read(path, depth+1, max_depth, include_file, pretty_print, ignore, pad + "     ")
+                _pretty_read(path, depth+1, max_depth, include_file, pad + "     ")
             else:
-                _recur_read(path, depth+1, max_depth, include_file, pretty_print, ignore, pad + "|    ")
+                _pretty_read(path, depth+1, max_depth, include_file, pad + "|    ")
         else:
             print(pad + "+--- " + file)
 
 
-def _regex_match(f, regex):
-    "Check if f matches the regex, return True if it is"
-    for i in regex:
-        if type(i) == str:
-            pat = re.compile(i)
-        else:
-            pat = i
-        if pat.match(f):
-            return True
-    return False
-
-
 if __name__ == "__main__":
-    tree(".")
+    tree("mock")
