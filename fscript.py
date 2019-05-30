ab86c688c53f720f9b005f8e3b50da8ee353f6e9
Daniel Xu <niellxu@yahoo.ca>
2019-05-30 10:58:02 -0400

    Implemented count function that count number of lines

--- fscript.py
+++ fscript.py
@@ -66,3 +66,45 @@ def _pretty_read(parent, depth, max_depth, include_file, pad):
             out += pad + "+--- " + file
             print(pad + "+--- " + file)
     return out
+
+
+def count(d, include_blank=False, recursive=True):
+    """
+    Count the number of lines in one file, if
+    the given file is a directory and recursive
+    is enabled, all the files and subfolders will
+    be counted as well.
+
+    `d` Directory or file
+    
+    `include_blank` Blank line counted as one line or not, default is False
+    
+    `recursive` Recursively count subfiles and folders if given file
+    is directory, default is enabled
+    """
+    if os.path.isdir(d):
+        if recursive:
+            return _countln_recursive(d, include_blank)
+        else:
+            raise Exception("count: " + str(d) + " is a directory but recursive is disabled")
+    return _countln(d)
+
+
+def _countln_recursive(file, include_blank):
+    if os.path.isfile(file):
+        return _countln(file, include_blank)
+    lines = 0
+    for f in os.listdir(file):
+        lines += _countln_recursive(os.path.join(file, f), include_blank)
+    return lines
+
+
+def _countln(file, include_blank):
+    with open(file) as f:
+        lines = f.readlines()
+    count = len(lines)
+    if not include_blank:
+        for l in lines:
+            if len(l.strip()) == 0:
+                count -= 1
+    return count
